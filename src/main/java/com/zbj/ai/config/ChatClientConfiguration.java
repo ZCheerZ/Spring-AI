618e68a3c22d7d5d78c752a86891925f749c55c8
ZBJ_ <905906892@qq.com>
2025-07-23 21:58:49 +0800

    会话功能实现，会话记忆以及会话历史展示出来

--- src/main/java/com/zbj/ai/config/ChatClientConfiguration.java
+++ src/main/java/com/zbj/ai/config/ChatClientConfiguration.java
@@ -1,8 +1,13 @@
 package com.zbj.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+//import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +15,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatClientConfiguration {
 
+//    @Bean //更新了可以直接注入得到 自己写成bean进行管理了 不是第三方bean
+//    public ChatMemory chatMemory() {
+//        // 创建一个简单的内存聊天记录
+//        return new InMemoryChatMemory();
+//    }
+    @Autowired
+    private ChatMemory chatMemory;
+
     @Bean
-    public ChatClient chatClient(OllamaChatModel ollamaChatModel) {
+    public ChatClient chatClient(OllamaChatModel ollamaChatModel,ChatMemory chatMemory) {
         return ChatClient.builder(ollamaChatModel)
-                .defaultSystem("我是小左，一个AI助手，擅长回答各种问题。")
-                .defaultAdvisors(new SimpleLoggerAdvisor())
-                .build();
+                .defaultSystem("你是小左，一个AI助手，擅长回答各种问题。")
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                ).build();
     }
 }
--- src/main/java/com/zbj/ai/controller/TestController.java
+++ src/main/java/com/zbj/ai/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zbj.ai.controller;
 
 
+import com.zbj.ai.repository.ChatHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-@RequiredArgsConstructor
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID; //比原来的AbstractchatMemoryAdvisor空出来一层
+
+
+@RequiredArgsConstructor //这里使用 Lombok 的 @RequiredArgsConstructor 注解来自动生成构造函数 private final需要构造函数才能注入
 @RestController
 @RequestMapping("/ai")
 public class TestController {
 
     private final ChatClient chatClient;
 
+    private final ChatHistoryRepository chatHistoryRepository;
+
+
     @RequestMapping(value = "/chat", produces = "text/html;charset=UTF-8")
-    public Flux<String> chat(String prompt) {
+    public Flux<String> chat(String prompt, String chatId) {
         // 使用 chatClient 进行聊天
-        Flux<String> content = chatClient.prompt().user(prompt).stream().content();
+        // 保存会话id 到内存 之后去数据库
+        chatHistoryRepository.save("chat", chatId);
+        // 使用 chatClient 进行聊天
+        Flux<String> content = chatClient.prompt().user(prompt)
+                .advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, chatId))
+                .stream().content();
         return content;
     }
 
